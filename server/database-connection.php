78eaced43749d0e7b038620a2177aabdbe88c6ff
Winch3ster <kingstonlee96@gmail.com>
2022-08-22 17:51:18 +0800

    modified connection

--- server/database-connection.php
+++ server/database-connection.php
@@ -1,12 +1,25 @@
 <?php
-
-
-$user = "root";
-$password = "";
-$database = "Bitsey";
-
 // Create connection
-$conn = new mysqli('localhost', $user, $password, $database);
+
+//$user = "root";
+//$password = "";
+//$database = "Bitsey";
+//$conn = new mysqli('localhost', $user, $password, $database);
+
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+
 
 // Check connection
 if ($conn->connect_error) {
