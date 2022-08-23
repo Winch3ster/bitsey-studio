1cc1ab54c11587d5aa24fc39dff131be7795c8cb
Winch3ster <kingstonlee96@gmail.com>
2022-08-23 15:59:01 +0800

    edited no man and r6

--- Javascript/game-function-user.js
+++ Javascript/game-function-user.js
@@ -54,7 +54,7 @@ function sendJSON(){
     xhr.open("POST", "../server/get-json.php");
     
     xhr.onreadystatechange = function() { if (xhr.readyState === 4 && xhr.status === 200) { console.log(xhr.responseText); } }
-    var data = JSON.stringify({ "gameName": `${gameName.innerText}`, "gameImageLink": gameImageLink, "platform": platformSelected, "amountToBuy": amountToBuy, "price": price });
+    var data = JSON.stringify({ "gameName": String(gameName.innerText), "gameImageLink": gameImageLink, "platform": platformSelected, "amountToBuy": amountToBuy, "price": price });
     xhr.setRequestHeader("Content-type", "application/json") // or "text/plain"
     xhr.send(data); 
 
